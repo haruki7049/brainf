b532d539ac01f759b3f17cb73dcc71ca506bd5ee
haruki7049 <tontonkirikiri@gmail.com>
2025-03-21 15:55:48 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use std::io::Read;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    execute_brainfuck(&input);
+}
+
+fn execute_brainfuck(code: &str) {
+    let mut memory = vec![0u8; 30000];
+    let mut pointer: usize = 0;
+    let mut pc: usize = 0;
+
+    let mut loop_stack = Vec::new();
+
+    while pc < code.len() {
+        match code.as_bytes()[pc] as char {
+            '>' => pointer += 1,
+            '<' => pointer -= 1,
+            '+' => memory[pointer] = memory[pointer].wrapping_add(1),
+            '-' => memory[pointer] = memory[pointer].wrapping_sub(1),
+            '.' => print!("{}", memory[pointer] as char),
+            ',' => {
+                let mut input = [0];
+                std::io::stdin().read_exact(&mut input).expect("Failed to read from stdin");
+                memory[pointer] = input[0];
+            }
+            '[' => {
+                if memory[pointer] == 0 {
+                    let mut open_brackets = 1;
+                    while open_brackets > 0 {
+                        pc += 1;
+                        match code.as_bytes()[pc] as char {
+                            '[' => open_brackets += 1,
+                            ']' => open_brackets -= 1,
+                            _ => {}
+                        }
+                    }
+                } else {
+                    loop_stack.push(pc);
+                }
+            }
+            ']' => {
+                if memory[pointer] != 0 {
+                    pc = *loop_stack.last().expect("Loop stack underflow");
+                } else {
+                    loop_stack.pop();
+                }
+            }
+            _ => {}
+        }
+        pc += 1;
+    }
 }
